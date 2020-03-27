9b0089d2d330527fd03bdfd605d188e9932a95ba
Jose7198 <jose719810@hotmail.com>
2020-03-27 16:16:11 -0500

    Clases

---
+++ App/app/src/main/java/com/example/proyectofragmentos/clases/Materia.java
@@ -0,0 +1,61 @@
+package com.example.proyectofragmentos.clases;
+
+import com.example.proyectofragmentos.vistas.MainActivity;
+
+import java.util.ArrayList;
+
+public class Materia {
+    private String codigo;
+    private String nombre;
+    private String profesor;
+    private ArrayList<Estudiante> estudiantesInscritos;
+
+    public Materia(String codigo, String nombre, String profesor){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.profesor = profesor;
+        this.estudiantesInscritos = new ArrayList<Estudiante>();
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public ArrayList<Estudiante> getEstudiantesInscritos() {
+        return estudiantesInscritos;
+    }
+
+    public void setEstudiantesInscritos(ArrayList<Estudiante> estudiantesInscritos) {
+        this.estudiantesInscritos = estudiantesInscritos;
+    }
+
+    public void agregarEstudiante(Estudiante estudiante){
+        estudiantesInscritos.add(estudiante);
+    }
+
+    public void quitarEstudiante(Estudiante estudiante){
+        estudiantesInscritos.remove(estudiante);
+    }
+
+    
+}
