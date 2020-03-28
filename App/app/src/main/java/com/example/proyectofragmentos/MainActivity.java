6e7495c744947a1595a777067b41008ea703ac86
JohaCR <caro.cerezor@gmail.com>
2020-03-28 16:54:47 -0500

    Button anadir materias y estudiantes

--- App/app/src/main/java/com/example/proyectofragmentos/MainActivity.java
+++ App/app/src/main/java/com/example/proyectofragmentos/MainActivity.java
@@ -38,18 +38,18 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+
 
         new AdaptadorArchivo().setContext(this.getApplicationContext());
         Singleton.getInstance();
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 
